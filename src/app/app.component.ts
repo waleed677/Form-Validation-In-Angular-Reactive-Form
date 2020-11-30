0abb45668ce054ea70332b553254cbaf983a1286
waleed677 <waleedmalik677@gmail.com>
2020-12-01 03:06:29 +0500

    first Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder , Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'registerationForm';
+  registerForm;
+
+  constructor(private fb:FormBuilder){}
+
+  ngOnInit(): void {
+    this.registerForm = FormGroup;
+    this.registerForm = this.fb.group({
+      name : ['', Validators.compose([Validators.required, Validators.minLength(5)])],
+      email : ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
+
+  }
+
+  submit(){
+    console.log(this.registerForm.value);
+    this.registerForm.reset();
+  }
+  get name(){
+    return this.registerForm.get('name');
+  }
+
+  get email(){
+    return this.registerForm.get('email');
+  }
+
+  get password(){
+    return this.registerForm.get('password');
+  }
+
 }
